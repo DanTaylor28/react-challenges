779653f707ccd64963072bdfd5a21bf0770f6328
DanTaylor28 <dan.taylor1493@gmail.com>
2022-11-22 09:55:43 +0000

    Convert my class based component to a functional one using hooks and an api get request

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,15 @@ import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 // import Content from './components/Content';
 // import ContentHooks from './components/ContentHooks';
-import ContentAPI from './components/ContentAPI';
+// import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from './components/ContentAPIHooks';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
       <NavBarForm />
-      <ContentAPI />
+      <ContentAPIHooks />
     </div>
   );
 }
